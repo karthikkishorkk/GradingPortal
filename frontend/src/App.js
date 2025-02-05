b9a04f2be0a45d1d12b0e8f315e375d7df19914e
ghanasyaam <ghanasyaaam@gmail.com>
2025-02-05 17:48:38 +0530

    Firestore connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage.tsx';
-
+import Adminpage from './Pages/AdminPage/Adminpage.tsx';
+import TeacherRegForm from './Pages/TeacherRegForm/TeacherRegForm.tsx';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <HomePage />,
+    },
+    {
+      path: '/admin',
+      element: <Adminpage />,
+    },
+    {
+      path: '/teacher-reg',
+      element: <TeacherRegForm />,
     },
   ]);
 
